c7e958b37c54c940360e13848d502b8c93c56408
hieu1509 <hhieu1592004@gmail.com>
2026-05-01 16:00:36 +0700

    save khach hang

---
+++ src/main/java/ViewModels/KhachHangViewModel.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ViewModels;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class KhachHangViewModel {
+       private int id;
+    private String maKhachHang;
+    private String soDienThoai;
+    private String hoTen;
+    
+    public KhachHangViewModel(){
+        
+    }
+    
+    public KhachHangViewModel(int id, String maKhachHang, String soDienThoai, String hoTen){
+        this.id=id;
+        this.maKhachHang=maKhachHang;
+        this.soDienThoai=soDienThoai;
+        this.hoTen=hoTen;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMaKhachHang() {
+        return maKhachHang;
+    }
+
+    public void setMaKhachHang(String maKhachHang) {
+        this.maKhachHang = maKhachHang;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+    
+}
